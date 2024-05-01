6277416bd95f82b1009716dd5b2d9827616b956d
Mücteba Tayfur <mtayfur@instechnologies.net>
2024-05-01 17:01:19 +0300

    fix: app module

--- Vitalpilot.Client/src/app/app.module.ts
+++ Vitalpilot.Client/src/app/app.module.ts
@@ -2,14 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { HomeComponent } from './pages/home/home/home.component';
-import { DoctorsComponent } from './pages/doctors/doctors.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    DoctorsComponent
   ],
   imports: [
     BrowserModule
